25c7581c821da52a96912ab7811867975a763e6d
Collin Prusse <collinprusse@mac.com>
2018-05-04 20:57:46 -0600

    Setup test database env

---
+++ src/firebase/firebase.js
@@ -0,0 +1,97 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+  };
+
+firebase.initializeApp(config);
+
+const database = firebase.database();
+
+export { firebase, database as default };
+
+
+
+
+
+
+
+// //child_removed
+// database.ref('expenses').on('child_removed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
+
+// //child_changed
+// database.ref('expenses').on('child_changed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
+
+// //child_added
+// database.ref('expenses').on('child_added', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
+
+// // database.ref('expenses')
+// //   .once('value')
+// //   .then((snapshot) => {
+// //       const expenses = [];
+
+// //       snapshot.forEach((childSnapshot) => {
+// //           expenses.push({
+//             id: childSnapshot.key,
+//             ...childSnapshot.val()
+//           });
+//       });
+//       console.log(expenses);
+//   });
+
+// database.ref('expenses').on('value', (snapshot) => {
+//     const expenses = [];
+
+//     snapshot.forEach((childSnapshot) => {
+//         expenses.push({
+//             id: childSnapshot.key,
+//             ...childSnapshot.val()
+//         });
+//     });
+//     console.log(expenses);
+// });
+
+// database.ref('expenses').push({
+//     description: 'Rent',
+//     note: '',
+//     amount: 1095400,
+//     createdAt: 39204209
+// });
+
+// database.ref().set({
+//     name: 'Dave',
+//     age: 27,
+//     isSingle: true,
+//     location: {
+//         city: 'Denver',
+//         country: 'USA'
+//     }
+// }).then(() => {
+//     console.log('Data is saved');
+// }).catch((e) => {
+//     console.log('This failed', e);
+// });
+
+// database.ref().update({
+//     name: 'Carl',
+//     age: 29
+// });
+
+// // database.ref('isSingle')
+// //     .remove()
+// //     .then(() => {
+// //         console.log('Data was removed');
+// //     }).catch((e) => {
+// //         console.log('Did not remove data', e);
+// //     });
